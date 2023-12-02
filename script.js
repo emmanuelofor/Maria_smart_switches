e0988191d434fe1a1a9bd49a237c2992d1abedaf
emmanuelofor <emmanuel.ofori@kibo.school>
2023-12-02 22:47:19 +0000

    maria smart switches

---
+++ script.js
@@ -0,0 +1,26 @@
+// // JavaScript for responsive navigation menu
+// function toggleMobileMenu() {
+//     const navLinks = document.querySelector(".nav-links");
+//     navLinks.classList.toggle("active");
+// }
+
+// // Attach event listener to the mobile menu icon/button
+// document.querySelector(".mobile-menu-icon").addEventListener("click", toggleMobileMenu);
+
+// Sample JavaScript function to display a welcome message
+function showWelcomeMessage() {
+    alert("Welcome to Maria Decor - Smart Home Solutions!");
+}
+
+// Attach event listener to a button or element, if needed
+document.getElementById("welcome-button").addEventListener("click", showWelcomeMessage);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    
+    mobileMenuToggle.addEventListener('click', function () {
+        mobileMenu.classList.toggle('active');
+    });
+});
